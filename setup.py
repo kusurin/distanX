df0dae9f6f71ad6a48f53b98113b6208bfe8d7c7
kusurin <kusu_rin@sjtu.edu.cn>
2025-06-17 18:38:12 +0800

    chore: 更新setup.py版本至0.2.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='distanX',
-    version='0.2.2',
+    version='0.2.3',
     packages=find_packages(),
     install_requires=['opencv-python', 'anndata', 'pandas', 'numpy', 'joblib'],
     author='kusurin',
